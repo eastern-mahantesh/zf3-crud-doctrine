350812cf356c9617ffe6656f4597a96b5c77913d
Mahantesh Policepatil <m.policepatil@easternenterprise.com>
2021-04-14 12:27:47 +0530

    Added DB Credentials

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,16 +1,30 @@
 <?php
 /**
- * Global Configuration Override
+ * Local Configuration Override
  *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
  *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
  */
 
+use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
+
 return [
-    // ...
+    'doctrine' => [
+        'connection' => [
+            'orm_default' => [
+                //'driverClass' => PDOMySqlDriver::class,
+                'params' => [
+                    'host'     => 'localhost',
+                    'user'     => 'root',
+                    'password' => 'root',
+                    'dbname'   => 'blog',
+                ]
+            ],
+        ],
+    ],
 ];
